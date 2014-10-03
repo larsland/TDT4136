aecda2bcdd6a010d806ee6c18e5b987492792dfb
trulsmp <trulmp@stud.ntnu.no>
2014-10-03 14:41:01 +0200

    cleanup

--- task2/astar2.py
+++ task2/astar2.py
@@ -1,16 +1,13 @@
 from heapq import heappush, heappop # for priority queue
 import math
 import time
-import Queue
-from Tkinter import *
 
 
 class node:
-    xPos = 0 # x position
-    yPos = 0 # y position
+    xPos, yPos = 0, 0 # x,y position
     cost = 0 # total distance already travelled to reach the node
     priority = 0 # priority = distance + remaining distance estimate
-    algorithm = 0
+    algorithm = 0 # determine what algorithm is in use
 
     def __init__(self, xPos, yPos, cost, priority,the_map, algorithm):
         self.xPos = xPos
@@ -22,19 +19,21 @@ class node:
             self.cost += cost
             self.getCost(the_map)
         self.algorithm = algorithm
-        print self.algorithm
 
     def __lt__(self, other): # comparison method for priority queue
         return self.priority < other.priority
 
+    #Calculate the priority based on distance from start and h-function if not dijekstra
     def updatePriority(self, xDest, yDest):
         if self.algorithm == '0' or self.algorithm == '1':
             self.priority = self.cost + self.hFunction(xDest, yDest) # A*
         if self.algorithm == '2':
             self.priority = self.cost
 
+    #Adds the cost of the node to the cost value
     def getCost(self, the_map):
         next_node_type = the_map[self.yPos][self.xPos]
+        # if we are using the bfs algorithm it makes all nodes the same value to find the shortest path
         if self.algorithm == '1':
             self.cost += 1
         elif next_node_type == "w":
@@ -47,39 +46,26 @@ class node:
             self.cost += 5
         elif next_node_type == "r":
             self.cost += 1
-        elif next_node_type == "#":
-            self.cost += 10000
 
-    # Estimate remaining distance to the goal.
+
+    # Estimate remaining distance to the goal using manhattan.
     def hFunction(self, xDest, yDest):
         xd = xDest - self.xPos
         yd = yDest - self.yPos
         d = abs(xd) + abs(yd)
         return(d)
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    OKGREY= '\x1b[30m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    RED = '\x1b[31;1m'
-    GREEN = '\x1b[36;1m'
-
 
 # A-star algorithm.
 # The path returned will be a string of digits of directions.
 def pathFind(the_map, n, m, dirs, dx, dy, xA, yA, xB, yB, algorithm):
-    print algorithm
-    closed_nodes_map = [] # map of closed (tried-out) nodes
-    open_nodes_map = [] # map of open (not-yet-tried) nodes
+    closed_nodes = [] # map of closed (tried-out) nodes
+    open_nodes = [] # map of open (not-yet-tried) nodes
     dir_map = [] # map of dirs
     row = [0] * n
     for i in range(m): # create 2d arrays
-        closed_nodes_map.append(list(row))
-        open_nodes_map.append(list(row))
+        closed_nodes.append(list(row))
+        open_nodes.append(list(row))
         dir_map.append(list(row))
 
     pq = [[], []] # priority queues of open (not-yet-tried) nodes
@@ -88,7 +74,7 @@ def pathFind(the_map, n, m, dirs, dx, dy, xA, yA, xB, yB, algorithm):
     n0 = node(xA, yA, 0, 0,the_map, algorithm)
     n0.updatePriority(xB, yB)
     heappush(pq[pqi], n0)
-    open_nodes_map[yA][xA] = n0.priority # mark it on the open nodes map
+    open_nodes[yA][xA] = n0.priority # mark it on the open nodes map
 
     # A* search
     while len(pq[pqi]) > 0:
@@ -99,8 +85,8 @@ def pathFind(the_map, n, m, dirs, dx, dy, xA, yA, xB, yB, algorithm):
         x = n0.xPos
         y = n0.yPos
         heappop(pq[pqi]) # remove the node from the open list
-        open_nodes_map[y][x] = 0
-        closed_nodes_map[y][x] = 1 # mark it on the closed nodes map
+        open_nodes[y][x] = 0
+        closed_nodes[y][x] = 1 # mark it on the closed nodes map
 
         # stop searching if we have reached the goal and generate the path
         if x == xB and y == yB:
@@ -111,25 +97,25 @@ def pathFind(the_map, n, m, dirs, dx, dy, xA, yA, xB, yB, algorithm):
                 path = c + path
                 x += dx[j]
                 y += dy[j]
-            return path, closed_nodes_map, open_nodes_map
+            return path, closed_nodes, open_nodes
 
-        # generate moves (child nodes) in all possible dirs
+        # generate moves (child nodes) in all possible directions
         for i in range(4):
             xdx = x + dx[i]
             ydy = y + dy[i]
-            if not (xdx < 0 or xdx > n-1 or ydy < 0 or ydy > m - 1 or closed_nodes_map[ydy][xdx] == 1 or the_map[ydy][xdx] == '#'):
+            if not (xdx < 0 or xdx > n-1 or ydy < 0 or ydy > m - 1 or closed_nodes[ydy][xdx] == 1 or the_map[ydy][xdx] == '#'):
                 # generate a child node
                 m0 = node(xdx, ydy, n0.cost, n0.priority,the_map, algorithm)
                 m0.updatePriority(xB, yB)
                 # if it is not in the open list then add into that
-                if open_nodes_map[ydy][xdx] == 0:
-                    open_nodes_map[ydy][xdx] = m0.priority
+                if open_nodes[ydy][xdx] == 0:
+                    open_nodes[ydy][xdx] = m0.priority
                     heappush(pq[pqi], m0)
                     # mark its parent node direction
                     dir_map[ydy][xdx] = (i + dirs / 2) % dirs
-                elif open_nodes_map[ydy][xdx] > m0.priority:
+                elif open_nodes[ydy][xdx] > m0.priority:
                     # update the priority
-                    open_nodes_map[ydy][xdx] = m0.priority
+                    open_nodes[ydy][xdx] = m0.priority
                     # update the parent direction
                     dir_map[ydy][xdx] = (i + dirs / 2) % dirs
                     # replace the node
@@ -165,42 +151,35 @@ def main():
     dirs = 4 # number of possible directions to move on the map
     dx = [1, 0, -1, 0]
     dy = [0, 1, 0, -1]
-
-    y = 0
-    x = 0
-    xA = 0
-    yA = 0
-    xB = 0
-    yB = 0
-
+    y,  x, xA, yA, xB, yB = 0,0,0,0,0,0
+    #Array the hold the entire board
     array = []
 
     #allows user to choose what map to use and adds the board to a array
     algorithm = chooseAlgo()
-    print algorithm
     map = raw_input("Choose a map between 1-4, or 21-24: ")
     board = open(map+".txt","r").readlines()
     for line in board:
         x = 0
-        preArray = []
+        subArray = []
         for char in line:
             if char == 'A':
                 xA = x
                 yA = y
-                preArray.append('A')
+                subArray.append('A')
             elif char == 'B':
                 xB = x
                 yB = y
-                preArray.append('B')
+                subArray.append('B')
             elif char == '.':
-                preArray.append('r')
+                subArray.append('r')
             elif char == '#':
-                preArray.append('#')
+                subArray.append('#')
             elif char != '\n':
-                preArray.append(char)
+                subArray.append(char)
             x += 1
         y += 1
-        array.append(preArray)
+        array.append(subArray)
 
 
     n = x-1 # horizontal size of the map
@@ -213,10 +192,8 @@ def main():
     print 'Start: ', xA, yA
     print 'Finish: ', xB, yB
     t = time.time()
-    print algorithm
+    # Call the search algorithm and return the closed and open nodes to be displayed
     route, closed_nodes, open_nodes = pathFind(array, n, m, dirs, dx, dy, xA, yA, xB, yB,algorithm)
-    print closed_nodes
-    print open_nodes
     print 'Time to generate the route (seconds): ', time.time() - t
     print 'Route: ' + route
     
@@ -285,6 +262,19 @@ def main():
                 print bcolors.WARNING + xy + bcolors.ENDC, # finish
 
         print
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    OKGREY= '\x1b[30m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    RED = '\x1b[31;1m'
+    GREEN = '\x1b[36;1m'
+
 
 
 main()
